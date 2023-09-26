14729de920c8b7cee8128bb1370750a0363139d9
Ashley Coles <ashley@acoles.co.uk>
2023-09-26 11:19:41 +0100

    Basic test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,19 +2,22 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    // Telling the test file to use our database migrations
+    use DatabaseMigrations;
 
+    public function test_getAll_success(): void
+    {
+        // $this->get sends a get request to the given URL and captures the response
+        $response = $this->get('/api/posts');
+
+        // Asserting that response has a 200 (success) status code
         $response->assertStatus(200);
     }
 }
